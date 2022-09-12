bad000a3b569e914934edbcf4811c03561e65fe2
Harlit Estevez <harlit.estevez@gmail.com>
2022-09-12 17:00:59 -0400

    se agrega tareaB

---
+++ Capitulo5_4_5/Program.cs
@@ -0,0 +1,12 @@
+﻿static double factorial(int num)
+{
+    if (num == 0 || num == 1)
+         return 1;
+    return num * factorial(num - 1);
+}
+
+
+Console.Write("Digite el valor del factorial: ");
+int Factorial = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("El Factorial de {0} es: {1}", Factorial, factorial(Factorial));
+
